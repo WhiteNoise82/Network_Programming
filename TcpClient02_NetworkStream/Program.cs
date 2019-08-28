be1553ed8d0cbb6003716fac0f60f6b28db4e346
OH SOUNG KWON <crystals8960@gmail.com>
2019-08-28 12:30:16 +0900

    NetworkStream

---
+++ TcpClient02_NetworkStream/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpClient02_NetworkStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient("192.168.0.11", 7);
+            if (tcpClient.Connected)
+            {
+                Console.WriteLine("서버 연결 성공");
+                NetworkStream ns = tcpClient.GetStream();
+                string Message = "Hello World!";
+                byte[] SendByteMessage = Encoding.ASCII.GetBytes(Message);
+                ns.Write(SendByteMessage, 0, SendByteMessage.Length);
+
+                byte[] ReceiveByteMessage = new byte[32];
+                ns.Read(ReceiveByteMessage, 0, 32);
+                string ReceiveMessage = Encoding.ASCII.GetString(ReceiveByteMessage);
+                Console.WriteLine(ReceiveMessage);
+                ns.Close();
+            }
+            else
+            {
+                Console.WriteLine("서버 연결 실패");
+            }
+
+            tcpClient.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TcpListener02_NetworkStream/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpListener02_NetworkStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Parse("192.168.0.11"), 7);
+            tcpListener.Start();
+            Console.WriteLine("대기상태");
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            NetworkStream ns = tcpClient.GetStream();
+            byte[] ReceiveMessage = new byte[100];
+            ns.Read(ReceiveMessage, 0, 100);
+            string strMessage = Encoding.ASCII.GetString(ReceiveMessage);
+            Console.WriteLine(strMessage);
+
+            string EchoMessage = "Hi~~";
+            byte[] SendMessage = Encoding.ASCII.GetBytes(EchoMessage);
+            ns.Write(SendMessage, 0, SendMessage.Length);
+            ns.Close();
+            tcpClient.Close();
+            tcpListener.Stop();
+            Console.ReadKey();
+        }
+    }
+}
